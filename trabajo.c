6e9d4406da4b62e86c45d59c02f5cb06dd8936ca
mrstevengz <ssequeira@uamv.edu.ni>
2024-04-04 19:01:19 -0600

    Codigo

---
+++ trabajo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /*Asignar una variable con mi nombre e imprimir el contenido de esa variable*/
+    char name [30]="Jan", "uary";
+    printf("%s\n", name);
+    return 0;
+}
